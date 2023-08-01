e7691da4176bb87f809f2eb329cfd8bdf3698c16
Muharax <adriangajda@o2.pl>
2023-08-01 03:19:32 +0200

    +update03:19

--- src/MainWindow/MainWindow.js
+++ src/MainWindow/MainWindow.js
@@ -7,53 +7,55 @@ import Search from "./Search/Search";
 import LogoutX from "../logout/logout";
 import { PrivateRoute } from "../PrivateRoute/PrivateRoute";
 import Siatka from "../siatka/siatka";
+import '../App.css';
 
-function MainWindow({ handleLogout, serverMessage, role}){
-
+function MainWindow({ handleLogout, serverMessage, role }) {
     const [alertMessage, setAlertMessage] = useState('null');
+    const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <Router>  {/* Router na najwyższym poziomie */}
+    const handleMenuToggle = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <Router>
             <LogoutX onLogout={handleLogout} />
             <div className="alert"></div>
-            <div className="MainWindow">  
+            <div className="MainWindow">
                 <div className="alert">
                     {serverMessage && <div>{serverMessage}</div>}
                 </div>
                 <div className="COL1">
                     <div className="A1 LOGO">
-                        <Link to="/Home">  {/* Dodanie linku do logo */}
+                        <Link to="/Home">
                             <img className="logo-img" src={logo} alt="Logo" />
                         </Link>
                     </div>
                     <div className="B1">
-                        <Search></Search>
+                        <Search />
                     </div>
                 </div>
-
                 <div className="COL2">
                     <div className="A2">
-                        <div className="MENU">
-                        
-                        <Link to="/Home">Home</Link>
-                        <Link to="/Lotto">Lotto</Link>
-                        <Link to="/Uzytkownicy">Uzytkownicy BEZ ADMIN</Link>
-                        {role === 'admin' && <Link to="/Uzytkownicy">Uzytkownicy</Link>}
-                         
+                        <button onClick={handleMenuToggle} className="menu-button">
+                            ☰
+                        </button>
+                        <div className={`MENU ${isOpen ? 'open' : ''}`}>
+                            <Link to="/Home">Home</Link>
+                            <Link to="/Lotto">Lotto</Link>
+                            <Link to="/Uzytkownicy">Uzytkownicy BEZ ADMIN</Link>
+                            {role === 'admin' && <Link to="/Uzytkownicy">Uzytkownicy</Link>}
                         </div>
                     </div>
                     <div className="B2 OKNO">
-                            <div id="content">
-                            
+                        <div id="content">
                             <Routes>
                                 <Route path="/" element={<Home />} />
                                 <Route path="/Home" element={<Home />} />
                                 <Route path="/Lotto" element={<Siatka />} />
                             </Routes>
-                                <PrivateRoute path="/Uzytkownicy" element={<Uzytkownicy />} setAlertMessage={setAlertMessage} />
-                            
-                            
-                            </div>
+                            <PrivateRoute path="/Uzytkownicy" element={<Uzytkownicy />} setAlertMessage={setAlertMessage} />
+                        </div>
                     </div>
                 </div>
             </div>
